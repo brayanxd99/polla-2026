20d4b472ec83fc8e30b3e4d4b888518d98a6368d
brayanxD99 <nachosneyder6@gmail.com>
2026-06-01 17:48:40 -0500

    fix: remover eslint obsoleto de next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Esto le dice a Vercel que ignore los errores de TypeScript
-    // y compile el proyecto pase lo que pase.
+    // Esto obliga a Vercel a ignorar cualquier error de tipos
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // De paso, ignoramos también advertencias pesadas de diseño de código
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
